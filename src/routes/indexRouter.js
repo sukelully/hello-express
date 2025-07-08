53df57d4b27a2ef29c4fa422077edd5686ab7cdc
Luke Sullivan <luke@sukelully.dev>
2025-07-08 12:59:19 +0100

    feat(router): add indexRouter.js

---
+++ src/routes/indexRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const indexRouter = Router();
+
+indexRouter.get("/", (req, res) => {
+  res.send("Welcome to the homepage!");
+});
+
+module.exports = indexRouter;
