145e63c417db666990d28fd6bfcc06e351e4a0f0
Luke Sullivan <luke@sukelully.dev>
2025-07-08 12:56:34 +0100

    feat(router): add bookRouter.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get('/', (req, res) => res.send('Hello, world!'));
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- src/routes/authorRouter.js
+++ src/routes/authorRouter.js
@@ -1,8 +1,8 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const authorRouter = Router();
 
-authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
+authorRouter.get('/', (req, res) => res.send('All authors'));
+authorRouter.get('/:authorId', (req, res) => {
   const { authorId } = req.params;
   res.send(`Author ID: ${authorId}`);
 });
